e79864722074b8f6f8e1670798571d081885ca9e
Daniel-FD <daniel@bryant.dental>
2023-01-25 13:36:13 +0100

    image_processing

---
+++ Tools/Image_Processing/f_measurents.py
@@ -0,0 +1,274 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import matplotlib.pyplot as plt
+import math
+import imutils
+from IPython import display
+import time
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+# 
+line_width = 10
+point_width = 10
+colour_line = (255, 0, 0)
+colour_point = (255, 0, 0)
+colour_rectangle = (255, 255, 255)
+colour_oval = (255, 255, 255)
+thickness_oval = 10
+
+# 
+def distanceCalculate(p):
+    p1 = p[0]
+    p2 = p[1]
+    dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return dis
+
+def angleLinePoints(p):
+    p1 = p[0]
+    p2 = p[1]
+    # 
+    p1_x = p1[0]
+    p1_y = p1[1]
+    p2_x = p2[0]
+    p2_y = p2[1]
+    # 
+    d_x = p2_x - p1_x
+    d_y = p2_y - p1_y
+    # 
+    angle_radians = np.arctan(d_y/d_x)
+    angle_degrees = math.degrees(angle_radians)
+    return angle_degrees
+
+def area_px_within_polyline(stacked_array):
+    x = [stacked_array[0] for stacked_array in stacked_array]
+    y = [stacked_array[1] for stacked_array in stacked_array]
+    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+
+
+def bgr_image(image):
+    """ The code takes in an image as input and splits it into three colors: 
+    blue (B), green (G), and red (R)."""
+    (B, G, R) = cv2.split(image)
+    return B, G, R
+
+def height_width_image(image):
+    """ The code will return the height and width of an image."""
+    height, width, _ = image.shape
+    return height, width
+
+def image_bgr_to_rgb(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+def image_rgb_to_bgr(image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image
+
+def face_mesh_points(image):
+    mp_face_mesh = mp.solutions.face_mesh
+    face_mesh = mp_face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5)
+    result = face_mesh.process(image)
+    height, width = height_width_image(image)
+    mesh_points= np.array([np.multiply([p.x, p.y], [width, height]).astype(int) for p in result.multi_face_landmarks[0].landmark])
+    return result, mesh_points
+
+def mesh_points_to_df(mesh_points):
+    df_mesh_points = pd.DataFrame(columns=['X_pos', 'Y_pos', 'idx'])
+    # points_x = [mesh_points[0] for stacked_array in mesh_points]
+    # points_y = [mesh_points[1] for stacked_array in mesh_points]
+    for i, point in enumerate(mesh_points):
+        point_x = point[0]
+        point_y = point[1]
+        df_mesh_points.at[i,'X_pos'] = point_x
+        df_mesh_points.at[i,'Y_pos'] = point_y
+        df_mesh_points.at[i,'idx'] = i
+    return df_mesh_points
+
+def mesh_points_interactive_plot(mesh_points):
+    df_mesh_points = mesh_points_to_df(mesh_points)
+    fig = px.scatter(df_mesh_points, y="Y_pos", x="X_pos",hover_data=['idx'])
+    fig.update_traces(marker_size=5)
+    fig['layout']['yaxis']['autorange'] = "reversed"
+    fig.write_html("output/landmarks_mesh.html")
+    return fig
+
+def mesh_points_interactive_plot_with_image(mesh_points, image_path):
+    image_temp = cv2.imread(image_path)
+    df_mesh_points = mesh_points_to_df(mesh_points)
+    height, width = height_width_image(image_temp)
+    # 
+    fig = go.Figure()
+    fig.add_layout_image(
+            x=0,
+            sizex=width,
+            y=0,
+            sizey=height,
+            xref="x",
+            yref="y",
+            opacity=1.0,
+            layer="below",
+            source=image_path
+    )
+    fig.add_scatter(x=df_mesh_points['X_pos'],y=df_mesh_points['Y_pos'],mode="markers",marker=dict(size=1, color="Red"))
+    fig.update_xaxes(showgrid=False, range=(0, width))
+    fig.update_yaxes(showgrid=False, scaleanchor='x', range=(height, 0))
+    fig.update_layout(xaxis_range=[0,width])
+    fig.write_html("output/landmarks_mesh_with_photo.html")
+    fig.write_image("output/landmarks_mesh_with_photo.png")
+    fig.write_image("output/landmarks_mesh_with_photo.svg")
+    fig.write_image("output/landmarks_mesh_with_photo.pdf")
+
+    return fig
+
+
+def print_face_mesh_image(image, result):
+    height, width = height_width_image(image)
+    for facial_landmarks in result.multi_face_landmarks:
+        for i in range(0, 468):
+            pt1 = facial_landmarks.landmark[i]
+            x = int(pt1.x * width)
+            y = int(pt1.y * height)
+            image = cv2.circle(image, (x, y), 1, colour_point, point_width)
+    return image
+
+
+def find_iris_location(mesh_points,landmarks):
+    (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(mesh_points[landmarks['rightEyeIris']])
+    (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[landmarks['leftEyeIris']])
+    center_left_iris = np.array([l_cx, l_cy], dtype=np.int32)
+    center_right_iris = np.array([r_cx, r_cy], dtype=np.int32)
+    iris_position = [center_left_iris,center_right_iris]
+    iris_radius = [l_radius, r_radius]
+    # 
+    return iris_position, iris_radius
+
+def print_iris_location(image, mesh_points, landmarks):
+    image = cv2.polylines(image, [mesh_points[landmarks['rightEyeIris']]], 1, colour_line, line_width)
+    image = cv2.polylines(image, [mesh_points[landmarks['leftEyeIris']]], 1, colour_line, line_width)
+    return image
+
+def print_center_iris(image, iris_position):
+    center_left_iris = iris_position[0]
+    center_right_iris = iris_position[1]
+    image = cv2.circle(image, center_left_iris, 1, colour_point, point_width)
+    image = cv2.circle(image, center_right_iris, 1, colour_point, point_width)
+    return image
+
+def print_line_left_to_right_iris(image, iris_position):
+    center_left_iris = iris_position[0]
+    center_right_iris = iris_position[1]
+    image = cv2.line(image, center_left_iris, center_right_iris, colour_line, line_width)
+    # 
+    return image
+
+# 
+def get_face_dimensions_px(mesh_points, landmarks):
+    width_face_px = distanceCalculate(mesh_points[landmarks['leftToRight']])
+    height_face_px = distanceCalculate(mesh_points[landmarks['topToBottom']])
+    return width_face_px, height_face_px
+
+def get_ipd_px(iris_position):
+    center_left_iris = iris_position[0]
+    center_right_iris = iris_position[1]
+    ipd_px = distanceCalculate([center_left_iris, center_right_iris])
+    return ipd_px
+# 
+def print_line_top_to_bottom(image, mesh_points, landmarks):
+    image = cv2.polylines(image, [mesh_points[landmarks['topToBottom']]],  1, colour_line, line_width)
+    return image
+
+def print_line_left_to_right(image, mesh_points, landmarks):
+    image = cv2.polylines(image, [mesh_points[landmarks['leftToRight']]],  1, colour_line, line_width)
+    return image
+
+def print_silhouette(image, mesh_points, landmarks):
+    image = cv2.polylines(image, [mesh_points[landmarks['silhouette']]],  1, colour_line, line_width)
+    return image
+
+def print_right_silhouette(image, mesh_points, landmarks):
+    image = cv2.polylines(image, [mesh_points[landmarks['rightSilhouette']]],  1, colour_line, line_width)
+    return image
+
+def area_px_right_silhoutte(mesh_points, landmarks):
+    right_silhoutte = mesh_points[landmarks['rightSilhouette']]
+    area_px_right_silhoutte = area_px_within_polyline(right_silhoutte)
+    return area_px_right_silhoutte
+
+def print_left_silhouette(image, mesh_points, landmarks):
+    image = cv2.polylines(image, [mesh_points[landmarks['leftSilhouette']]],  1, colour_line, line_width)
+    return image
+
+def area_px_left_silhoutte(mesh_points, landmarks):
+    left_silhoutte = mesh_points[landmarks['leftSilhouette']]
+    area_px_left_silhoutte = area_px_within_polyline(left_silhoutte)
+    return area_px_left_silhoutte
+
+def get_top_to_bottom_angle(mesh_points, landmarks):
+    top_to_bottom_angle = angleLinePoints(mesh_points[landmarks['topToBottom']])
+    return top_to_bottom_angle
+
+def get_left_to_right_angle(mesh_points, landmarks):
+    left_to_right_angle = angleLinePoints(mesh_points[landmarks['leftToRight']])
+    return left_to_right_angle
+
+def print_rectangle_card_area(image, mesh_points, landmarks):
+    _, height_face_px = get_face_dimensions_px(mesh_points, landmarks)
+    x_start = mesh_points[landmarks['outerRightEyebrowUpper']][0]
+    y_start = mesh_points[landmarks['outerRightEyebrowUpper']][1] - 0.6*height_face_px
+    x_end, y_end =  mesh_points[landmarks['outerLeftEyebrowUpper']]
+    start_point = (int(x_start), int(y_start))
+    end_point = (int(x_end), int(y_end))
+    image = cv2.rectangle(image, start_point, end_point, colour_rectangle, line_width)
+    return image
+
+def print_face_oval(image):
+    height, width = height_width_image(image)
+    image = cv2.ellipse(image, center=(int(width/2), int(height/2)), axes=(int(min(width,height)/4),int(min(width,height)/3)), angle=0, startAngle=0, endAngle=360, color=colour_oval, thickness=thickness_oval)  
+    return image
+
+# ---------------------------------
+# SCREENPRINTING
+# ---------------------------------
+
+# Printing text information onscreen
+x_position_0 = 50
+colour_text =  (255, 255, 0)
+colour_text_valid = (0,255,0)
+colour_text_invalid = (255, 0, 0)
+font_scale = 8
+font_thickness = 10
+# 
+
+def screenprint_top_to_bottom_angle(image, top_to_bottom_angle, top_to_bottom_angle_ref, top_to_bottom_angle_max_deviation):
+    if abs(top_to_bottom_angle - top_to_bottom_angle_ref) < top_to_bottom_angle_max_deviation:
+        colour_text_top_to_bottom_angle = colour_text_valid
+    else:
+        colour_text_top_to_bottom_angle = colour_text_invalid
+    image = cv2.putText(image, f'top_to_bottom_angle: {str(round(top_to_bottom_angle,3))} [degrees]', (x_position_0,500), cv2.FONT_HERSHEY_PLAIN, font_scale, colour_text_top_to_bottom_angle, font_thickness)
+    return image
+
+def screenprint_left_to_right_angle(image, left_to_right_angle, left_to_right_angle_ref, left_to_right_angle_max_deviation_perc):
+    if abs(left_to_right_angle - left_to_right_angle_ref) < left_to_right_angle_max_deviation_perc:
+        colour_text_left_to_right_angle = colour_text_valid
+    else:
+        colour_text_left_to_right_angle = colour_text_invalid
+    image = cv2.putText(image, f'left_to_right_angle: {str(round(left_to_right_angle,3))} [degrees]', (x_position_0,800), cv2.FONT_HERSHEY_PLAIN, font_scale, colour_text_left_to_right_angle, font_thickness)
+    return image
+
+def screenprint_ipd_px(image, ipd_px):
+    image = cv2.putText(image, f'ipd_px: {str(round(ipd_px,3))} [px]', (50,1100), cv2.FONT_HERSHEY_PLAIN, font_scale, colour_text, font_thickness)
+    return image
+
+def screenprint_area_right_to_left_silhoutte(image, area_right_to_left_silhoutte, area_ratio_right_to_left_ref, area_ratio_right_to_left_max_deviation_perc):
+    if abs(area_right_to_left_silhoutte - area_ratio_right_to_left_ref) < area_ratio_right_to_left_max_deviation_perc:
+        colour_text_area_right_to_left_silhoutte = colour_text_valid
+    else:
+        colour_text_area_right_to_left_silhoutte = colour_text_invalid
+    image = cv2.putText(image, f'area_right_to_left_silhoutte: {str(round(area_right_to_left_silhoutte,3))} [%]', (x_position_0,1400), cv2.FONT_HERSHEY_PLAIN, font_scale,colour_text_area_right_to_left_silhoutte, font_thickness)
+    return image
--- Tools/Image_Processing/gradio_app/app.py
+++ Tools/Image_Processing/gradio_app/app.py
@@ -1,140 +1,44 @@
 import gradio as gr
-from flask import Flask, Response
+import requests
+from io import BytesIO
+import base64
+import matplotlib.image as mpimg
 import cv2
 import numpy as np
-import mediapipe as mp
-import matplotlib.pyplot as plt
-import math
-from IPython import display
-# --------------------
-MAX_ANGLE = 10
-# --------------------
-# --------------------
-# --------------------
-# --------------------
-def distanceCalculate(p):
-    p1 = p[0]
-    p2 = p[1]
-    dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
-    return dis
-# --------------------
-def angleLinePoints(p):
-    p1 = p[0]
-    p2 = p[1]
-    # 
-    p1_x = p1[0]
-    p1_y = p1[1]
-    p2_x = p2[0]
-    p2_y = p2[1]
-    # 
-    d_x = p2_x - p1_x
-    d_y = p2_y - p1_y
-    # 
-    angle_radians = np.arctan(d_y/d_x)
-    angle_degrees = math.degrees(angle_radians)
-    return angle_degrees
-# --------------------
-def process_image(image):
-    if isinstance(image, str):
-        image = cv2.imread(image)
-    # 
-    (B, G, R) = cv2.split(image)
-    mp_face_mesh = mp.solutions.face_mesh
-    face_mesh = mp_face_mesh.FaceMesh(
-        static_image_mode=True,
-        max_num_faces=1,
-        refine_landmarks=True,
-        min_detection_confidence=0.5)
-    result = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    height, width, _ = image.shape
-    mesh_points= np.array([np.multiply([p.x, p.y], [width, height]).astype(int) for p in result.multi_face_landmarks[0].landmark])
-    # Landmark mapping
-    landmarks = {
-        'topToBottom': [10, 152],
-        'leftToRight': [234, 454],
-        'rightEyeIris': [473, 474, 475, 476, 477],
-        'leftEyeIris': [468, 469, 470, 471, 472],
-        'outerRightEyebrowUpper': 70,
-        'outerLeftEyebrowUpper': 300,
-        'silhouette': [10,  338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
-        397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
-        172, 58,  132, 93,  234, 127, 162, 21,  54,  103, 67,  109]}
-    # Iris location
-    (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(mesh_points[landmarks['rightEyeIris']])
-    (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[landmarks['leftEyeIris']])
-    center_left_iris = np.array([l_cx, l_cy], dtype=np.int32)
-    center_right_iris = np.array([r_cx, r_cy], dtype=np.int32)
-    # Face dimension
-    width_face_px = distanceCalculate(mesh_points[landmarks['leftToRight']])
-    height_face_px = distanceCalculate(mesh_points[landmarks['topToBottom']])
-    # IPD calculation
-    ipd = distanceCalculate([center_left_iris, center_right_iris])
-    # Photo Quality Check
-    vertical_angle = angleLinePoints(mesh_points[landmarks['topToBottom']])
-    horizonal_angle = angleLinePoints(mesh_points[landmarks['leftToRight']])
-    # Rectangle for Card Area
-    x_start = mesh_points[landmarks['outerRightEyebrowUpper']][0]
-    y_start = mesh_points[landmarks['outerRightEyebrowUpper']][1] - 0.6*height_face_px
-    x_end, y_end =  mesh_points[landmarks['outerLeftEyebrowUpper']]
-    start_point = (int(x_start), int(y_start))
-    end_point = (int(x_end), int(y_end))
-    # 
-    overlay = image.copy()
-    alpha = 0.1
-    beta = (1.0 - alpha)
-    # Plot all the information
-    # plt.imshow(image)
-    # plt.imshow(image_landmarks)
-    for facial_landmarks in result.multi_face_landmarks:
-        for i in range(0, 468):
-            pt1 = facial_landmarks.landmark[i]
-            x = int(pt1.x * width)
-            y = int(pt1.y * height)
-            cv2.circle(image, (x, y), 1, (0,255,0), -1)
-    plt.imshow(cv2.polylines(image, [mesh_points[landmarks['topToBottom']]], 1, (0,255,0), 1))
-    plt.imshow(cv2.polylines(image, [mesh_points[landmarks['leftToRight']]], 1, (0,255,0), 1))
-    plt.imshow(cv2.polylines(image, [mesh_points[landmarks['silhouette']]], 1, (0,255,0), 1))
-    plt.imshow(cv2.line(image, center_left_iris, center_right_iris, (0, 255, 0), 2))
-    plt.imshow(cv2.line(image, mesh_points[landmarks['outerLeftEyebrowUpper']], mesh_points[landmarks['outerRightEyebrowUpper']], (0, 255, 0), 1))
-    plt.imshow(cv2.rectangle(image, start_point, end_point, (255,0,0), 1))
-    plt.imshow(cv2.ellipse(image, center=(int(width/2), int(height/2)), axes=(int(min(width,height)/4),int(min(width,height)/3)), angle=0, startAngle=0, endAngle=360, color=(255,255,255), thickness=4))    
-    plt.imshow(cv2.putText(image, f'IPD: {str(ipd)} [px]', (50,50), cv2.FONT_HERSHEY_PLAIN,1, (0,0,0), 1))
-    plt.imshow(cv2.putText(image, f'Face Height: {str(height_face_px)} [px]', (50,100), cv2.FONT_HERSHEY_PLAIN,1, (0,0,0),1))
-    plt.imshow(cv2.putText(image, f'Face Width: {str(width_face_px)} [px]', (50,150), cv2.FONT_HERSHEY_PLAIN,1, (0,0,0), 1))
-    if abs(horizonal_angle) < MAX_ANGLE:
-        plt.imshow(cv2.putText(image, f'Horizonal Angle: {str(horizonal_angle)} [degrees]', (50,200), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0), 1))
-        cv2.rectangle(overlay, (0,0), (width, height), (0, 255, 0), -1)  
-        image = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
-    elif horizonal_angle < -MAX_ANGLE:
-        plt.imshow(cv2.arrowedLine(image,[int(x_start) - (int(min(width,height)/4)),int(y_start+ height_face_px/2)], [int(x_start), int(y_start+ height_face_px/2)], (0, 255, 0), 4))
-        plt.imshow(cv2.putText(image, f'Horizonal Angle: {str(horizonal_angle)} [degrees]', (50,200), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255), 1))
-        cv2.rectangle(overlay, (0,0), (width, height), (0, 0, 255), -1)  
-        image = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
-        plt.imshow(image)
-    elif horizonal_angle > MAX_ANGLE:
-        plt.imshow(cv2.arrowedLine(image, [int(x_start), int(y_start+ height_face_px/2)],[int(x_start) - (int(min(width,height)/4)),int(y_start+ height_face_px/2)], (0, 255, 0), 4))
-        plt.imshow(cv2.putText(image, f'Horizonal Angle: {str(horizonal_angle)} [degrees]', (50,200), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255), 1))
-        cv2.rectangle(overlay, (0,0), (width, height), (0, 0, 255), -1)  
-        image = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
-        plt.imshow(image)
-    if abs(vertical_angle) > (90 - MAX_ANGLE): #[-80, 80] -> okay
-        plt.imshow(cv2.putText(image, f'Vertical Angle: {str(vertical_angle)} [degrees]', (50,250), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0), 1))
-    else:         
-        plt.imshow(cv2.putText(image, f'Vertical Angle: {str(vertical_angle)} [degrees]', (50,250), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255), 1))
-    # 
-    return image
-# --------------------
-# --------------------
-# --------------------
-# --------------------
-def processing_frame(im):
-    image = process_image(im)
-    return image
-# 
+
+def img_to_string(img):
+    # Encodes an image into a base64_string
+    _, encoded_img = cv2.imencode('.PNG', img)
+    base64_string = base64.b64encode(encoded_img).decode('utf-8')
+    return base64_string
+
+def string_to_img(base64_string):
+    # Decodes a base64_string into an image
+    imgdata = base64.b64decode(base64_string + '==')
+    im = BytesIO(imgdata)
+    img = mpimg.imread(im, format='PNG')
+    opencv_img= cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+    return opencv_img
+
+def api_image_processing(image):
+    # API Endpoint
+    url = "http://13.42.31.235/fmc_api"
+    # Step 1
+    b64_string = img_to_string(image)
+    #  Step 2
+    payload ={"base64_string": b64_string}
+    response = requests.post(url=url, data=payload) 
+    base64_string_resp = response.json()['message']
+    # Step 3
+    img_output = string_to_img(base64_string_resp)
+    # Return processed image
+    return img_output
+
 demo = gr.Interface(
-    processing_frame, 
+    api_image_processing, 
     gr.Image(source="webcam", streaming=True), 
     "image",
     live=True
 )
-demo.launch()
+
+demo.launch(debug=True)
